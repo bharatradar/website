0ee9ba5f3ce3927653f997fda48ecc7cdf77bd6c
ragavellur <raga.vellur@gmail.com>
2026-05-05 03:11:32 +0530

    feat: make dark mode default for docs site with BharatRadar landing page color palette

--- assets/js/darkmode-init.js
+++ assets/js/darkmode-init.js
@@ -1,21 +1,3 @@
-const globalDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-const localMode = localStorage.getItem('theme');
-
-if (globalDark && (localMode === null)) {
-
-  localStorage.setItem('theme', 'dark');
-  document.documentElement.setAttribute('data-dark-mode', '');
-
-}
-
-if (globalDark && (localMode === 'dark')) {
-
-  document.documentElement.setAttribute('data-dark-mode', '');
-
-}
-
-if (localMode === 'dark') {
-
-  document.documentElement.setAttribute('data-dark-mode', '');
-
-}
+// Always default to dark mode for BharatRadar
+document.documentElement.setAttribute('data-dark-mode', '');
+localStorage.setItem('theme', 'dark');
